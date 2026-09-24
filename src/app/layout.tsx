0a564382c22f2aab8d76a1736e7538f207a053a1
Shiva Bhusal <srollins175@gmail.com>
2026-09-24 12:16:27 +0545

    feat: Add Google Tag Manager integration to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import ClientShell from "../components/Layout/ClientShell";
 import {
@@ -79,6 +80,29 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        {/* Google Tag Manager  */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WRT3VSLT');
+          `}
+        </Script>
+
+        {/* Google Tag Manager - noscript */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WRT3VSLT"
+            height="0"
+            width="0"
+            style={{
+              display: "none",
+              visibility: "hidden",
+            }}
+          ></iframe>
+        </noscript>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
